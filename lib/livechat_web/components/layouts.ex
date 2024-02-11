21912eb06681095fcc73d7a4a702cb4f22b1ca9d
agustinesco <agustinesco@outlook.es>
2024-02-11 03:12:42 -0300

    Add helper function to sidebar

--- lib/livechat_web/components/layouts.ex
+++ lib/livechat_web/components/layouts.ex
@@ -2,4 +2,7 @@ defmodule LivechatWeb.Layouts do
   use LivechatWeb, :html
 
   embed_templates "layouts/*"
+
+  def maybe_active(%{room_id: room_id}, room_id), do: "active"
+  def maybe_active(_, _), do: ""
 end
